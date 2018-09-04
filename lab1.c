0acb996e8d101154d397b46b827bb2ea547c4110
namling46 <42969363+namling46@users.noreply.github.com>
2018-09-04 09:29:17 -0400

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -1,5 +1,5 @@
 void main(){
-int a, b, c, d, e;
+int a, b, c, closea, closeb, readCount, writeCount, writeCountABC, tempCount1, tempCount2;
 char buffer[150];
 a = creat("XYZ",0600);
 b = open("Downloads/test.file",0);
@@ -7,19 +7,34 @@ if(b<0){
 	printf("error with open");
 	exit(0);
 }
-c = read(b,buffer,150);
-if(c<0){
-	printf("error with read");
-	exit(0);
+readCount = 150;
+while(readCount<= 150) {
+	readCount = read(b,buffer,150);
+	if(readCount<0){
+		printf("error with read");
+		exit(0);
+	}
+	tempCount1 += readCount;
+	writeCount = write(a, buffer, readCount);
+	if(writeCount < 0) {
+		printf("error with write");
+		exit(0);
+	}
+	writeCountABC = write(a, "ABC", 3);
+	if(writeCountABC < 0) {
+		printf("error with write");
+		exit(0);
+	}
+	tempCount2 += readCount + 3;
 }
 printf("%s\n", buffer);
-d = close(b);
-if(d<0) {
+closeb = close(b);
+if(closeb<0) {
 	printf("error with close");
 	exit(0);
 }
-e = close(a);
-if(e<0) {
+closea= close(a);
+if(closea <0) {
 	printf("error with close");
 	exit(0);
 }
